8de8353bc2d6db913592b0340ffc6ec0d8257266
manik.saluja@in.abb.com <manik.saluja@in.abb.com>
2026-07-21 13:06:48 +0000

    Changes for the cfin gov dashboard

--- webapp/controller/ZView1.controller.js
+++ webapp/controller/ZView1.controller.js
@@ -1,10 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+], (Controller, MessageBox,MessageToast) => {
     "use strict";
 
     return Controller.extend("cfin.custom.zblocklistgovern.controller.ZView1", {
         onInit() {
+            this._oView = this.getView();
+            this._oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+        },
+        onPressSendForApproval:function(oEvent){
+            var oTable = this._oView.byId("idMappingTable"),
+                oSelectedItems = oTable.getSelectedItems();
+            if(oSelectedItems.length > 0){  
+                MessageToast.show(this._oResourceBundle.getText("xmsg.Message2"));
+            }else{
+                MessageBox.error(this._oResourceBundle.getText("xmsg.Message1"));
+            }
         }
     });
 });
